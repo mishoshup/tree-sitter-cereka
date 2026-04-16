b29e59e1bfd1e5db33496216ca0b184252a6c419
Danial Haikal <202131799+mishoshup@users.noreply.github.com>
2026-04-16 18:44:04 +0800

    fix: use node type syntax for standalone keyword rules in highlights.scm
    stop_bgm, save_menu, load_menu, end are named rules — query them as
    (stop_bgm) @keyword not "stop_bgm" @keyword which is for anonymous tokens

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,41 +1,43 @@
 
 ;; =====================
-;; Keywords
+;; Keywords (inline anonymous strings in sequences)
 ;; =====================
 
-"say"       @keyword
-"narrate"   @keyword
-"bg"        @keyword
-"char"      @keyword
-"hide"      @keyword
-"bgm"       @keyword
-"stop_bgm"  @keyword
-"sfx"       @keyword
-"label"     @keyword
-"jump"      @keyword
-"call"      @keyword
-"include"   @keyword
-"end"       @keyword
-"menu"      @keyword
-"button"    @keyword
-"goto"      @keyword
-"exit"      @keyword
-"set"       @keyword
-"if"        @keyword
-"endif"     @keyword
-"fade"      @keyword
-"save_menu" @keyword
-"load_menu" @keyword
-"save"      @keyword
-"load"      @keyword
-"ui"        @keyword
+"say"      @keyword
+"narrate"  @keyword
+"bg"       @keyword
+"char"     @keyword
+"hide"     @keyword
+"bgm"      @keyword
+"sfx"      @keyword
+"label"    @keyword
+"jump"     @keyword
+"call"     @keyword
+"include"  @keyword
+"menu"     @keyword
+"button"   @keyword
+"goto"     @keyword
+"exit"     @keyword
+"set"      @keyword
+"if"       @keyword
+"endif"    @keyword
+"fade"     @keyword
+"save"     @keyword
+"load"     @keyword
+"ui"       @keyword
+
+;; Named rules that are standalone keywords (must use node type syntax)
+(stop_bgm)  @keyword
+(save_menu) @keyword
+(load_menu) @keyword
+(end)       @keyword
 
 ;; =====================
 ;; UI elements & positions
 ;; =====================
 
-(ui_element)  @type
-(position)    @constant
+(ui_element) @type
+(position)   @constant
 
 ;; =====================
 ;; Operators
@@ -69,13 +71,13 @@
 ;; Filenames
 ;; =====================
 
-(bg_stmt file: (filename)    @string.special)
-(menu_bg file: (filename)    @string.special)
-(char_stmt file: (filename)  @string.special)
-(bgm file: (filename)        @string.special)
-(sfx file: (filename)        @string.special)
-(call file: (filename)       @string.special)
-(include file: (filename)    @string.special)
+(bg_stmt file: (filename)     @string.special)
+(menu_bg file: (filename)     @string.special)
+(char_stmt file: (filename)   @string.special)
+(bgm file: (filename)         @string.special)
+(sfx file: (filename)         @string.special)
+(call file: (filename)        @string.special)
+(include file: (filename)     @string.special)
 (ui_property path: (filename) @string.special)
 
 ;; =====================
